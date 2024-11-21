37dba2551cb3b665fae39434956d8cf587ea8793
Alexander Berridge <alexjberridge@gmail.com>
2024-11-21 09:13:51 +0000

    Adding TODO note about possible map style deprecation

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -392,6 +392,7 @@ class MapPageState extends State<MapPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
+        // TODO: Possible deprecation of styles in March 2025 (See: https://www.atlist.com/blog/json-map-styles-will-stop-working-march-2025)
         style: mapStyle,
         mapType: mapType,
         rotateGesturesEnabled: false,
