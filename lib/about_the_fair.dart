c7edfae9dd2cedbb9a8ee051c121fb8070f0d132
Matt Whiting <github@mattwhiting.com>
2025-10-20 16:57:07 +0100

    Did as I was told by the machine, to stop the machine telling me off

--- lib/about_the_fair.dart
+++ lib/about_the_fair.dart
@@ -9,7 +9,7 @@ class TextImageRow extends StatelessWidget {
   final double textWidthProportion;
   final bool imageOnLeft;
 
-  TextImageRow({required this.textSpan, required this.imagePath, required this.textWidthProportion, this.imageOnLeft = false});
+  const TextImageRow({super.key, required this.textSpan, required this.imagePath, required this.textWidthProportion, this.imageOnLeft = false});
 
   double _measureTextHeight(TextSpan textSpan, double maxWidth) {
     final TextPainter textPainter = TextPainter(
@@ -34,14 +34,14 @@ class TextImageRow extends StatelessWidget {
               width: constraints.maxWidth * (1 - textWidthProportion),
               child: Image.asset(imagePath, fit: BoxFit.contain),
             ),
-            Container(
+            SizedBox(
               width: constraints.maxWidth * textWidthProportion,
               child: Text.rich(
                 textSpan,
               )
             ),
           ] : [
-            Container(
+            SizedBox(
               width: constraints.maxWidth * textWidthProportion,
               child: Text.rich(
                 textSpan,
@@ -73,16 +73,16 @@ class AboutTheFairPage extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              TextImageRow(
-                textSpan: const TextSpan(text: 'Mill Road Winter Fair is a celebration of community along one of the most diverse and vibrant roads in Cambridge. Usually held on the first Saturday of December, the Fair brings together local businesses and organisations, shops and stallholders, musicians, artists and dancers in one day of festival joy.',),
+              const TextImageRow(
+                textSpan: TextSpan(text: 'Mill Road Winter Fair is a celebration of community along one of the most diverse and vibrant roads in Cambridge. Usually held on the first Saturday of December, the Fair brings together local businesses and organisations, shops and stallholders, musicians, artists and dancers in one day of festival joy.',),
                 imagePath: "assets/aboutPage/MRWF25_people_hat.png",
                 textWidthProportion: 0.75,
               ),
               const SizedBox(height: 12),
               Text('The 2025 Fair will be on Saturday 6th December, 10.30am to 4.30pm.', style: bodyStyle.copyWith(fontWeight: FontWeight.bold)),
               const SizedBox(height: 12),
-              TextImageRow(
-                textSpan: const TextSpan(text: 'Be sure to come early so that you don’t miss out; we have so much to see! The fire engine pull, opening ceremony, dance show and parade are all in the morning, as well as lots of amazing stalls, performance and events.',),
+              const TextImageRow(
+                textSpan: TextSpan(text: 'Be sure to come early so that you don’t miss out; we have so much to see! The fire engine pull, opening ceremony, dance show and parade are all in the morning, as well as lots of amazing stalls, performance and events.',),
                 imagePath: "assets/aboutPage/MRWF25_people_cake.png",
                 textWidthProportion: 0.67,
                 imageOnLeft: true,
@@ -103,7 +103,7 @@ class AboutTheFairPage extends StatelessWidget {
                               builder: (BuildContext context) => emailDetailsDialog(),
                             );
                           }),
-                    const TextSpan(text: ' if you’d like to be involved, or if you have a questions or ideas about the Fair.'),
+                    const TextSpan(text: ' if you’d like to be involved, or if you have ideas or questions about the Fair.'),
                   ],
                 ),
                 imagePath: "assets/aboutPage/MRWF25_people_sax.png",
@@ -112,16 +112,16 @@ class AboutTheFairPage extends StatelessWidget {
               const SizedBox(height: 12),
               Text('We also have a number of Fringe events throughout the year, with which we’d love you to get involved.', style: bodyStyle),
               const SizedBox(height: 12),
-              TextImageRow(
-                textSpan: const TextSpan(text: 'We are grateful for the support of our generous sponsors: Bush & Co Sales and Lettings (lead sponsor), Anglia Ruskin University, Hughes Hall, Taank Optometrists, Regalstar catering and Love Mill Road. The Mill Road Winter Fair is supported by a Cambridge City Council Community Grant and the Mill Road Traders Association.',),
+              const TextImageRow(
+                textSpan: TextSpan(text: 'We are grateful for the support of our generous sponsors: Bush & Co Sales and Lettings (lead sponsor), Anglia Ruskin University, Hughes Hall, Taank Optometrists, Regalstar catering and Love Mill Road. The Mill Road Winter Fair is supported by a Cambridge City Council Community Grant and the Mill Road Traders Association.',),
                 imagePath: "assets/aboutPage/MRWF25_people_juggle.png",
                 textWidthProportion: 0.75,
                 imageOnLeft: true,
               ),
               const SizedBox(height: 12),
               FittedBox(
-                child: Image.asset("assets/aboutPage/MRWF25_sponsor_logos.png"),
                 fit: BoxFit.fill,
+                child: Image.asset("assets/aboutPage/MRWF25_sponsor_logos.png"),
               ),
               const SizedBox(height: 12),
             ],
