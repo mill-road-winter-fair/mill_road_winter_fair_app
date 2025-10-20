67ddd1f05bb97e1bdef40a04ea35c8e2ce87ca05
Matt Whiting <github@mattwhiting.com>
2025-10-20 12:29:05 +0100

    Initial blank version, with email dialog moved and functionised in main.dart so it can be re-used here

--- lib/about_the_fair.dart
+++ lib/about_the_fair.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
+import 'package:mill_road_winter_fair_app/main.dart';
 
 class AboutTheFairPage extends StatelessWidget {
   const AboutTheFairPage({super.key});
@@ -6,6 +9,7 @@ class AboutTheFairPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     debugPrint('AboutTheFairPage build() called');
+    var bodyStyle = TextStyle(fontSize: 14, color: Theme.of(context).colorScheme.onSecondary);
     return Scaffold(
       appBar: AppBar(title: const Text("About Mill Road Winter Fair")),
       body: SingleChildScrollView(
@@ -13,32 +17,35 @@ class AboutTheFairPage extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              const Text(
-                  "Mill Road Winter Fair is a celebration of community along one of the most diverse and vibrant roads in Cambridge. Usually held on the first Saturday of December, the Fair brings together local businesses and organisations, shops and stallholders, musicians, artists and dancers in one day of festival joy."),
-              const SizedBox(height: 20),
-              ConstrainedBox(
-                constraints: const BoxConstraints(maxHeight: 200),
-                child: CarouselView(
-                  itemExtent: MediaQuery.of(context).size.width - 80,
-                  shrinkExtent: MediaQuery.of(context).size.width - 80,
-                  elevation: 3.0,
-                  itemSnapping: true,
+              const Text("Mill Road Winter Fair is a celebration of community along one of the most diverse and vibrant roads in Cambridge. Usually held on the first Saturday of December, the Fair brings together local businesses and organisations, shops and stallholders, musicians, artists and dancers in one day of festival joy."),
+              const SizedBox(height: 12),
+              Text('The 2025 Fair will be on Saturday 6th December, 10.30am to 4.30pm.', style: bodyStyle.copyWith(fontWeight: FontWeight.bold)),
+              const SizedBox(height: 12),
+              Text('Be sure to come early so that you don’t miss out; we have so much to see! The fire engine pull, opening ceremony, dance show and parade are all in the morning, as well as lots of amazing stalls, performance and events.', style: bodyStyle),
+              const SizedBox(height: 12),
+              Text.rich(
+                TextSpan(
                   children: [
-                    Image.asset('assets/aboutPage/carousel01.jpg', fit: BoxFit.cover),
-                    Image.asset('assets/aboutPage/carousel02.jpg', fit: BoxFit.cover),
-                    Image.asset('assets/aboutPage/carousel03.jpg', fit: BoxFit.cover),
-                    Image.asset('assets/aboutPage/carousel04.jpg', fit: BoxFit.cover),
+                    const TextSpan(text: 'There are lots of opportunities to volunteer for the Fair – from communications, finding performers, setting stuff up, stewarding and just plain old organising – so do '),
+                    TextSpan(
+                        text: 'get in touch',
+                        style: const TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            HapticFeedback.lightImpact();
+                            showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => emailDetailsDialog(),
+                            );
+                          }),
+                    const TextSpan(text: ' if you’d like to be involved, or if you have a questions or ideas about the Fair.'),
                   ],
                 ),
               ),
-              const SizedBox(height: 20),
-              const Text('The 2025 Fair will be on Saturday 6th December, 10.30am to 4.30pm.', style: TextStyle(fontWeight: FontWeight.bold)),
-              const SizedBox(height: 20),
-              const Text('Be sure to come early so that you don’t miss out! We have so much to see! The fire engine pull, opening ceremony, dance show and parade are all in the morning, as well as lots of amazing stalls, performance and events.'),
-              const SizedBox(height: 8),
-              const Text('There are lots of opportunities to volunteer for the Fair – from communications, finding performers, setting stuff up, stewarding and just plain old organising – so do get in touch if you’d like to get involved. If you have a question about or idea for the 2025 Fair, please email us.'),
-              const SizedBox(height: 8),
-              const Text('We also have a number of Fringe events throughout the year, which we’d love you to get involved with.'),
+              const SizedBox(height: 12),
+              Text('We also have a number of Fringe events throughout the year, with which we’d love you to get involved.', style: bodyStyle),
+              const SizedBox(height: 12),
+              Text('We are grateful for the support of our generous sponsors: Bush & Co Sales and Lettings (lead sponsor), Anglia Ruskin University,  Hughes Hall, Taank Optometrists,  Regalstar catering and Love Mill Road. The Mill Road Winter Fair is supported by a Cambridge City Council Community Grant and the Mill Road Traders Association.', style: bodyStyle),
             ],
           ), // Add event details here
         ),
