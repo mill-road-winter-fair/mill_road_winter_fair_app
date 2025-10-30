c440e2f886d5f8173e37324d1c49bac38a8519bd
Alexander Berridge <alexjberridge@gmail.com>
2025-10-30 09:40:35 +0000

    Optimising imports

--- lib/important_info_page.dart
+++ lib/important_info_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:mill_road_winter_fair_app/main.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ImportantInfoPage extends StatelessWidget {
   const ImportantInfoPage({super.key});
