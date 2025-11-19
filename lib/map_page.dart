b721ccad1dca5eb00363c8db6e8b8cbd2ae7c145
Matt Whiting <matt@mattwhiting.com>
2025-11-19 19:36:29 +0000

    Fixed automerge regression so maximum modal height is once again 90% of screen

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -536,7 +536,7 @@ void addGroupMarker(listing) async {
                   builder: (context, constraints) {
                     return ConstrainedBox(
                       constraints: BoxConstraints(
-                        maxHeight: constraints.maxHeight * 0.95,
+                        maxHeight: constraints.maxHeight * 0.90,
                       ),
                       child: Scrollbar(
                         controller: groupSheetModalScrollController,
@@ -669,7 +669,7 @@ void addGroupMarker(listing) async {
                   }
                   return ConstrainedBox(
                     constraints: BoxConstraints(
-                      maxHeight: constraints.maxHeight * 0.95,
+                      maxHeight: constraints.maxHeight * 0.90,
                     ),
                     child: Scrollbar(
                       controller: specificSheetModalScrollController,
