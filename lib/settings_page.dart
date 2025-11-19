310ad53ed280ec08718145e8f60edf49154902f0
Matt Whiting <github@mattwhiting.com>
2025-11-19 12:48:10 +0000

    Removed unused package

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'dart:io';
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
