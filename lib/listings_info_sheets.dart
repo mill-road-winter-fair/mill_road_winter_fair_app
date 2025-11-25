a6228156c008167428ea6a5f652f835f93e44ada
Matt Whiting <github@mattwhiting.com>
2025-11-25 19:47:31 +0000

    Fixing git mess

--- lib/listings_info_sheets.dart
+++ lib/listings_info_sheets.dart
@@ -5,7 +5,7 @@ import 'package:mill_road_winter_fair_app/get_current_location.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 // Hardcoded fair date for 2025; change this today's date for testing
-final fairDate = DateTime(2025, 11, 25);
+final fairDate = DateTime(2025, 12, 6);
 
 // Function to determine if the event has ended based on endTime string
 bool hasEventEnded(String endTime) {
