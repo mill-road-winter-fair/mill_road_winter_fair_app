0f54834e0fdd35ad58508907f43b154ee25a3200
Alexander Berridge <alexjberridge@gmail.com>
2026-08-21 23:01:29 +0100

    Adding a new test to check the new behaviour of the notifications

--- test/listing_may_change_reminder_test.dart
+++ test/listing_may_change_reminder_test.dart
@@ -31,5 +31,39 @@ void main() {
       expect(listingUpdateNoticeEnabled, isFalse);
       onTest = true;
     });
+
+    testWidgets(
+      'shows the Fair-day and post-Fair notices even when listings notice is dismissed',
+      (WidgetTester tester) async {
+        SharedPreferences.setMockInitialValues({
+          ListingUpdateNotifier.preferenceKey: false,
+        });
+        onTest = false;
+        listingUpdateNoticeEnabled = false;
+
+        await tester.pumpWidget(const MaterialApp(home: Scaffold(body: SizedBox())));
+        final context = tester.element(find.byType(SizedBox));
+
+        for (final noticeDate in [
+          fairDate,
+          fairDate.add(const Duration(days: 1)),
+        ]) {
+          final showNotice = ListingUpdateNotifier.maybeShowNotice(
+            context,
+            now: noticeDate,
+          );
+          await tester.pumpAndSettle();
+
+          expect(find.text('Listings may change'), findsOneWidget);
+          expect(find.text("Don't show this again"), findsNothing);
+
+          await tester.tap(find.text('Okay'));
+          await tester.pumpAndSettle();
+          await showNotice;
+        }
+
+        onTest = true;
+      },
+    );
   });
 }
